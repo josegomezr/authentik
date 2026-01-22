95ec362402240a3101bec4ea7da54c015ab8828c
Jose D. Gomez R. <jose.gomez@suse.com>
2026-01-22 18:08:54 +0100

    feat: use authentik's config machinery for pulling values
    `authentik.lib.config.ConfigLoader` class reports when it cannot parse
    the value alongside the exception message.

--- suse_settings/user_settings.py
+++ suse_settings/user_settings.py
@@ -1,11 +1,8 @@
 # User-defined settings
-import os
-import json
+from authentik.lib.config import CONFIG
 
-AWS_S3_OBJECT_PARAMETERS = {}
+# S3 Configuration
+AWS_S3_OBJECT_PARAMETERS = CONFIG.get_dict_from_b64_json("suse.aws_s3_object_parameters", {})
+CONFIG.log("info", "Loaded AWS_S3_OBJECT_PARAMETERS={}".format(AWS_S3_OBJECT_PARAMETERS))
 
-if 'AWS_S3_OBJECT_PARAMETERS' in os.environ:
-    try:
-        AWS_S3_OBJECT_PARAMETERS = json.loads(os.environ['AWS_S3_OBJECT_PARAMETERS'])
-    except json.decoder.JSONDecodeError:
-        pass
+CONFIG.log("info", "Loaded user_settings")
